7a8cf8bb36680b7069da2a28746a24300653beb8
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-22 21:39:46 +0200

    add role view

---
+++ Models/roleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mvc_train.Models
+{
+    public class roleViewModel
+    {
+        public string? roleId { get; set; }
+        public string? roleName { get; set; }
+        public bool useRole { get; set; }
+    }
+}
