ebc7e9d98b70bf9271b986a2c37938a5e6967d6b
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-22 21:25:27 +0200

    add email interface

---
+++ Data/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿namespace Mvc_train.Data
+{
+    public interface IEmailSender
+    {
+        public interface IEmpRepo : IDataRepository<Employee>
+        {
+            void setPayRoll(Employee employee);
+
+            decimal getSalary(Employee employee);
+        }
+    }
+}
