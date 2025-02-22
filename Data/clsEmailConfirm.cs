1c4a2b444cb492bdf0988084b48f7126f322acf1
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-22 21:25:12 +0200

    add emailconfirmation class

---
+++ Data/clsEmailConfirm.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Mvc_train.Data
+{
+    public class clsEmailConfirm : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var fMail = "yourmail";
+            var fPassword = "yourpassword";
+
+            var theMsg = new MailMessage();
+            theMsg.From = new MailAddress(fMail);
+            theMsg.Subject = subject;
+            theMsg.To.Add(email);
+            theMsg.Body = $"<html><body>{htmlMessage}</body></html>";
+            theMsg.IsBodyHtml = true;
+
+            var smtpClint = new SmtpClient("smtp-mail.outlook.com")
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(fMail, fPassword),
+                Port = 587
+            };
+
+            smtpClint.Send(theMsg);
+        }
+    }
+}
