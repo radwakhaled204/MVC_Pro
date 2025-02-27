09c26f8cdc11d27c2ddd2b32775d4ba0fc9b9091
Radwa Khaled <radwaabouzed@gmail.com>
2025-02-27 21:08:22 +0200

    add comment

--- Models/roleViewModel.cs
+++ Models/roleViewModel.cs
@@ -4,6 +4,6 @@
     {
         public string? roleId { get; set; }
         public string? roleName { get; set; }
-        public bool useRole { get; set; }
+        public bool useRole { get; set; }//3
     }
 }
