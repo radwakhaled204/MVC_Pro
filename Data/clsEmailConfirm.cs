faf2f7cf31ee2b96ac50a595ecbf695c88cd7f04
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-05 21:19:20 +0200

    update pro

--- Data/clsEmailConfirm.cs
+++ Data/clsEmailConfirm.cs
@@ -5,26 +5,26 @@ namespace Mvc_train.Data
 {//3
     public class clsEmailConfirm : IEmailSender
     {
-        //public async Task SendEmailAsync(string email, string subject, string htmlMessage)
-        //{
-        //    var fMail = "yourmail";
-        //    var fPassword = "yourpassword";
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var fMail = "yourmail";
+            var fPassword = "yourpassword";
 
-        //    var theMsg = new MailMessage();
-        //    theMsg.From = new MailAddress(fMail);
-        //    theMsg.Subject = subject;
-        //    theMsg.To.Add(email);
-        //    theMsg.Body = $"<html><body>{htmlMessage}</body></html>";
-        //    theMsg.IsBodyHtml = true;
+            var theMsg = new MailMessage();
+            theMsg.From = new MailAddress(fMail);
+            theMsg.Subject = subject;
+            theMsg.To.Add(email);
+            theMsg.Body = $"<html><body>{htmlMessage}</body></html>";
+            theMsg.IsBodyHtml = true;
 
-        //    var smtpClint = new SmtpClient("smtp-mail.outlook.com")
-        //    {
-        //        EnableSsl = true,
-        //        Credentials = new NetworkCredential(fMail, fPassword),
-        //        Port = 587
-        //    };
+            var smtpClint = new SmtpClient("smtp-mail.outlook.com")
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(fMail, fPassword),
+                Port = 587
+            };
 
-        //    smtpClint.Send(theMsg);
-        //}
+            smtpClint.Send(theMsg);
+        }
     }
 }
