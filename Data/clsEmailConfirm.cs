dfd81ca73e4f34577fbe9a8fc2d2ce2345722ec0
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-07 19:56:06 +0200

    update

--- Data/clsEmailConfirm.cs
+++ Data/clsEmailConfirm.cs
@@ -2,7 +2,7 @@
 using System.Net;
 
 namespace Mvc_train.Data
-{
+{// 9
     public class clsEmailConfirm : IEmailSender
     {
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
