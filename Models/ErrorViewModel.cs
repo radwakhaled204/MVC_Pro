860132f66500bc4a432688ac88ffecc117d25677
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-14 22:59:23 +0200

    Update pro

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 namespace Mvc_train.Models
 {
     public class ErrorViewModel
-    { //error model
+    { 
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
