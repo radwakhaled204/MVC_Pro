2ee744d35d79f5b5230eee890795c7572e819598
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-14 22:56:14 +0200

    Update pro

--- Models/roleViewModel.cs
+++ Models/roleViewModel.cs
@@ -1,8 +1,9 @@
 ﻿namespace Mvc_train.Models
 {
     public class roleViewModel
-    {
+    { // Role
         public string? roleId { get; set; }
+        
         public string? roleName { get; set; }
         public bool useRole { get; set; }
     }
