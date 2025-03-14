ecc50b423510dab9797e89c106ca795b9e1fdeda
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-14 22:55:42 +0200

    Update pro

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -4,78 +4,78 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mvc_train.Controllers
 {
- //[Authorize(Roles = clsRoles.roleAdmin)]
- //public class RolesController : Controller
- //{
+    [Authorize(Roles = clsRoles.roleAdmin)]
+    public class RolesController : Controller
+    {
 
-    //        public RolesController(UserManager<IdentityUser> user, RoleManager<IdentityRole> roles)
-    //        {
-    //            _user = user;
-    //            _roles = roles;
-    //        }
+        public RolesController(UserManager<IdentityUser> user, RoleManager<IdentityRole> roles)
+        {
+            _user = user;
+            _roles = roles;
+        }
 
-    //        private readonly UserManager<IdentityUser> _user;
-    //        private readonly RoleManager<IdentityRole> _roles;
+        private readonly UserManager<IdentityUser> _user;
+        private readonly RoleManager<IdentityRole> _roles;
 
-    //        public async Task<IActionResult> Index()
-    //        {
-    //            var _users = await _user.Users.ToListAsync();
-    //            return View(_users);
-    //        }
+        public async Task<IActionResult> Index()
+        {
+            var _users = await _user.Users.ToListAsync();
+            return View(_users);
+        }
 
-    //        public async Task<IActionResult> addRoles(string userId)
-    //        {
-    //            var user = await _user.FindByIdAsync(userId);
-    //            var userRoles = await _user.GetRolesAsync(user);
+        public async Task<IActionResult> addRoles(string userId)
+        {
+            var user = await _user.FindByIdAsync(userId);
+            var userRoles = await _user.GetRolesAsync(user);
 
-    //            var allRoles = await _roles.Roles.ToListAsync();
-    //            if (allRoles != null)
-    //            {
-    //                var roleList = allRoles.Select(r => new roleViewModel()
-    //                {
-    //                    roleId = r.Id,
-    //                    roleName = r.Name,
-    //                    useRole = userRoles.Any(x => x == r.Name)
-    //                });
-    //                ViewBag.userName = user.UserName;
-    //                ViewBag.userId = userId;
-    //                return View(roleList);
-    //            }
-    //            else
-    //                return NotFound();
-    //        }
+            var allRoles = await _roles.Roles.ToListAsync();
+            if (allRoles != null)
+            {
+                var roleList = allRoles.Select(r => new roleViewModel()
+                {
+                    roleId = r.Id,
+                    roleName = r.Name,
+                    useRole = userRoles.Any(x => x == r.Name)
+                });
+                ViewBag.userName = user.UserName;
+                ViewBag.userId = userId;
+                return View(roleList);
+            }
+            else
+                return NotFound();
+        }
 
-    //        [HttpPost]
-    //        [ValidateAntiForgeryToken]
-    //        public async Task<IActionResult> addRoles(string userId, string jsonRoles)
-    //        {
-    //            var user = await _user.FindByIdAsync(userId);
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> addRoles(string userId, string jsonRoles)
+        {
+            var user = await _user.FindByIdAsync(userId);
 
-    //            List<roleViewModel> myRoles =
-    //                JsonConvert.DeserializeObject<List<roleViewModel>>(jsonRoles);
+            List<roleViewModel> myRoles =
+                JsonConvert.DeserializeObject<List<roleViewModel>>(jsonRoles);
 
-    //            if (user != null)
-    //            {
-    //                var userRoles = await _user.GetRolesAsync(user);
+            if (user != null)
+            {
+                var userRoles = await _user.GetRolesAsync(user);
 
-    //                foreach (var role in myRoles)
-    //                {
-    //                    if (userRoles.Any(x => x == role.roleName.Trim()) && !role.useRole)
-    //                    {
-    //                        await _user.RemoveFromRoleAsync(user, role.roleName.Trim());
-    //                    }
+                foreach (var role in myRoles)
+                {
+                    if (userRoles.Any(x => x == role.roleName.Trim()) && !role.useRole)
+                    {
+                        await _user.RemoveFromRoleAsync(user, role.roleName.Trim());
+                    }
 
-    //                    if (!userRoles.Any(x => x == role.roleName.Trim()) && role.useRole)
-    //                    {
-    //                        await _user.AddToRoleAsync(user, role.roleName.Trim());
-    //                    }
-    //                }
+                    if (!userRoles.Any(x => x == role.roleName.Trim()) && role.useRole)
+                    {
+                        await _user.AddToRoleAsync(user, role.roleName.Trim());
+                    }
+                }
 
-    //                return RedirectToAction(nameof(Index));
-    //            }
-    //            else
-    //                return NotFound();
-    //        }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+                return NotFound();
+        }
 
-    //}
+    }
 }
