f982d5fbdf7166e59319dfad9ee80ab35fd3ecf4
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-14 22:55:48 +0200

    Update pro

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace Mvc_train.Data
         public DbSet<Item> Items { get; set; }
         public DbSet<mvcCategory> MvcCategories { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        //QUARY
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<mvcCategory>().HasData(
