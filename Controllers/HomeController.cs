e2f864774415448068fbaa46417e06370d72d17b
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-18 19:12:58 +0200

    Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Mvc_train.Models;
 
 namespace Mvc_train.Controllers
-{ //HomeController
+{ 
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
