119a1806131df886bef59dcba9e19fdf1732281d
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-18 19:13:08 +0200

    Update

--- Data/IEmailSender.cs
+++ Data/IEmailSender.cs
@@ -2,7 +2,7 @@
 {
     public interface IEmailSender
     {
-        //EMAIL
+       
         //public interface IEmpRepo : IDataRepository<Employee>
         //{
         //    void setPayRoll(Employee employee);
