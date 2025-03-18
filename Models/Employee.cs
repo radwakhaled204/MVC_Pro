8ba4a78e3d84c488be28ef144e81364b20a02a54
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-18 19:16:45 +0200

    Update

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Mvc_train.Models
 {
     public class Employee
@@ -13,6 +13,7 @@ namespace Mvc_train.Models
         [Required(ErrorMessage = "Please Enter The Phone")]
         public string? EmployeePhone { get; set; }
         [Required(ErrorMessage = "Please Enter Verfied Email")]
+        [Column("email")]
         public string? EmployeeEmail { get; set; }
 
         public int? EmployeeAge { get; set; }
