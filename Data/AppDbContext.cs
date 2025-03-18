1edafaec35495e683b9152f355675d3cab22fe65
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-18 19:13:19 +0200

    Update

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,7 +2,7 @@
 using Mvc_train.Models;
 
 namespace Mvc_train.Data
-{
+{//AppDbContext
     public class AppDbContext: DbContext
     {
         public AppDbContext(DbContextOptions <AppDbContext> options):base(options)
