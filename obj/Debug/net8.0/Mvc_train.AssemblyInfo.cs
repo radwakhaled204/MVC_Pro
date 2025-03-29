e8d4d3638169c4c7feefa5b33e967d444117332f
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-29 22:39:30 +0200

    new DataAnnotations

--- obj/Debug/net8.0/Mvc_train.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mvc_train.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvc_train")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba29c605a52bb3454d8587d40d7b3a57da74f27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba4a78e3d84c488be28ef144e81364b20a02a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvc_train")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvc_train")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
