2a2ecbc2ce9482b5d5ad21ece9eeb86cd1fbaf87
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-29 23:57:13 +0200

    DataAnnotations

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -7,7 +7,7 @@ namespace Mvc_train.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Pleas")]
+        [Required(ErrorMessage = "Please")]
         public string? EmployeeName { get; set; }
 
         [Required(ErrorMessage = "Please Enter The Phone")]
